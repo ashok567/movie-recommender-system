e2799f2ea0e41c87e600696933795a837b1f32c0
Ashok Thakur <iashok180@gmail.com>
2019-10-09 01:34:36 +0530

    same mpvie issue fixed

--- recommender_system.py
+++ recommender_system.py
@@ -24,24 +24,25 @@ df['combined_features'] = df.apply(combine_features, axis=1)
 
 
 # Create count matrix and compute the Cosine Similarity
-cv = CountVectorizer()
-count_matrix = cv.fit_transform(df["combined_features"])
-cosine_sim = cosine_similarity(count_matrix)
+def similarity(movie_name):
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df["combined_features"])
+    cosine_sim = cosine_similarity(count_matrix)
+    movie_index = df[df['title'] == movie_name]['index'].values[0]
+    similar_movies = list(enumerate(cosine_sim[movie_index]))
+    similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+    return similar_movies
 
 
 movie_name = input("Enter a movie name\n").upper()
 if movie_name not in df['title'].tolist():
     print("The movie is not listed in our database")
 else:
-    movie_index = df[df['title'] == movie_name]['index'].values[0]
-
-    similar_movies = list(enumerate(cosine_sim[movie_index]))
-    similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
-
-    i = 1
     print("Also add these movies to your watch list: ")
-    for element in similar_movies:
-        if i < 11:
+    similar_movies_list = similarity(movie_name)
+    i = 0
+    for element in similar_movies_list[1:]:
+        if i < 10:
             print(df[df.index == element[0]]["title"].values[0])
         else:
             break
