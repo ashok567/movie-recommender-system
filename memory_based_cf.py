0eecbb97ba1f9c09a046cf0e1f3e8b49d3633ba5
Ashok Thakur <iashok180@gmail.com>
2019-10-10 13:57:16 +0530

    comments added

--- memory_based_cf.py
+++ memory_based_cf.py
@@ -1,3 +1,4 @@
+# Memory-based models are based on similarity between items or users using cosine-similarity.
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -8,12 +9,12 @@ df = pd.read_csv('data/u.data', sep='\t', names=columns)
 titles_df = pd.read_csv('data/Movie_Id_Titles')
 df = pd.merge(df, titles_df, on='item_id')
 
-train_data, test_data = train_test_split(df, test_size=0.25)
-
 users_counts = df['user_id'].nunique()
 movies_counts = df['item_id'].nunique()
+print("m: "+str(users_counts), "& n: "+str(movies_counts))
 
 # initialize the metrices
+train_data, test_data = train_test_split(df, test_size=0.25)
 train_data_matrix = np.zeros((users_counts, movies_counts))
 test_data_matrix = np.zeros((users_counts, movies_counts))
 
@@ -23,21 +24,24 @@ for index, row in train_data.iterrows():
 for index, row in test_data.iterrows():
     test_data_matrix[row['user_id']-1, row['item_id']-1] = row['rating']
 
-
+# train_data_matrix --> users * items(m*n)
 user_similarity = cosine_similarity(train_data_matrix)
+# train_data_matrix --> items * users(n*m)
 movie_similarity = cosine_similarity(train_data_matrix.T)
 
 
-def predict(ratings, similarity, type='user'):
-    if type == 'user':
+def predict(ratings, similarity, type1):
+    if type1 == 'user':
         mean_user_rating = ratings.mean(axis=1)
         ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
         pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff)/np.array([np.abs(similarity).sum(axis=1)]).T
-    elif type == 'movie':
+    elif type1 == 'movie':
         pred = ratings.dot(similarity)/np.array([np.abs(similarity).sum(axis=1)])
     return pred
 
 
-item_prediction = predict(train_data_matrix, movie_similarity, type='movie')
-user_prediction = predict(train_data_matrix, user_similarity, type='user')
+user_prediction = predict(train_data_matrix, user_similarity, 'user')
+item_prediction = predict(train_data_matrix, movie_similarity, 'movie')
 
+print(item_prediction)
+print(user_prediction)
