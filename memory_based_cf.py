fdd06a11df8dfe743964dd653fd13252a6e086bf
Ashok Thakur <iashok180@gmail.com>
2019-10-10 18:16:09 +0530

    model based cf configured

--- memory_based_cf.py
+++ memory_based_cf.py
@@ -13,20 +13,22 @@ duplicate_movies = duplicate_movies[['movieId', 'title']]
 duplicate_ids = duplicate_movies['movieId'].values.tolist()
 
 # review on duplicate movies
-duplicate_ratings = pd.DataFrame(ratings[ratings['movieId'].isin(duplicate_ids)]['movieId'].value_counts()).reset_index()
+duplicate_ratings = pd.DataFrame(ratings[ratings['movieId'].isin(
+    duplicate_ids)]['movieId'].value_counts()).reset_index()
 duplicate_ratings.columns = ['movieId', 'count']
 
 duplicated_df = pd.merge(duplicate_movies, duplicate_ratings, on='movieId')
-duplicate_ratings.sort_values(by='count', ascending=False)
 duplicated_df.sort_values(by=['title', 'count'], ascending=[True, False])
 
 # Removing duplicated ids with low review count
-low_ids = duplicated_df.drop_duplicates(subset='title', keep='last', inplace=False)['movieId']
+low_ids = duplicated_df.drop_duplicates(
+    subset='title', keep='last', inplace=False)['movieId']
 movies = movies.loc[~movies['movieId'].isin(low_ids)]
 ratings = ratings.loc[~ratings['movieId'].isin(low_ids)]
 
 df = pd.merge(ratings, movies, on='movieId')
-pivot_df = pd.pivot_table(df, index='title', columns=['userId'], values='rating')
+pivot_df = pd.pivot_table(df, index='title', columns=[
+                          'userId'], values='rating')
 movie = 'Toy Story'
 
 
@@ -35,9 +37,11 @@ def item_based_cf(pivot_df, movie):
     pivot_df = pivot_df.fillna(0)
     sparse_pivot = sparse.csr_matrix(pivot_df)
     recommender = cosine_similarity(sparse_pivot)
-    recommender_df = pd.DataFrame(recommender, columns=pivot_df.index, index=pivot_df.index)
+    recommender_df = pd.DataFrame(
+        recommender, columns=pivot_df.index, index=pivot_df.index)
     # print(recommender_df.index.str.upper().tolist())
-    cosine_df = pd.DataFrame(recommender_df[movie].sort_values(ascending=False))[1:6]
+    cosine_df = pd.DataFrame(
+        recommender_df[movie].sort_values(ascending=False))[1:6]
     cosine_df.reset_index(inplace=True)
     print(cosine_df['title'].values.tolist())
 
@@ -47,8 +51,10 @@ def user_based_cf(pivot_df, userid):
     pivot_df = pivot_df.fillna(0)
     sparse_pivot = sparse.csr_matrix(pivot_df)
     recommender = cosine_similarity(sparse_pivot)
-    recommender_df = pd.DataFrame(recommender, columns=pivot_df.index, index=pivot_df.index)
-    cosine_df = pd.DataFrame(recommender_df[userid].sort_values(ascending=False))[1:6]
+    recommender_df = pd.DataFrame(
+        recommender, columns=pivot_df.index, index=pivot_df.index)
+    cosine_df = pd.DataFrame(
+        recommender_df[userid].sort_values(ascending=False))[1:6]
     cosine_df.reset_index(inplace=True)
     print(cosine_df['userId'].values.tolist())
 
--- model_based_cf.py
+++ model_based_cf.py
@@ -1,29 +1,46 @@
-# Model-based CF is based on matrix factorization using SVD to factorize the matrix.
+# Model-based CF is based on matrix factorization using SVD
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from scipy import sparse
 from scipy.sparse.linalg import svds
 
-columns = ['user_id', 'item_id', 'rating', 'timestamp']
-df = pd.read_csv('data/u.data', sep='\t', names=columns)
-titles_df = pd.read_csv('data/Movie_Id_Titles')
-df = pd.merge(df, titles_df, on='item_id')
+movies = pd.read_csv('data/movies.csv')
+ratings = pd.read_csv('data/ratings.csv')
 
-users_counts = df['user_id'].nunique()
-movies_counts = df['item_id'].nunique()
-print("m: "+str(users_counts), "& n: "+str(movies_counts))
+movies['title'] = movies['title'].apply(lambda x: ' '.join(x.split(' ')[:-1]))
+# duplicate movie titles
+duplicate_movies = movies.groupby('title').filter(lambda x: len(x) >= 2)
+duplicate_movies = duplicate_movies[['movieId', 'title']]
 
-# initialize the metrices
-train_data, test_data = train_test_split(df, test_size=0.25)
-train_data_matrix = np.zeros((users_counts, movies_counts))
-test_data_matrix = np.zeros((users_counts, movies_counts))
+duplicate_ids = duplicate_movies['movieId'].values.tolist()
 
-sparsity = round(1.0-len(df)/(users_counts*movies_counts), 3)*100
-print("sparsity: "+str(sparsity)+"%")
+# review on duplicate movies
+duplicate_ratings = pd.DataFrame(ratings[ratings['movieId'].isin(
+    duplicate_ids)]['movieId'].value_counts()).reset_index()
+duplicate_ratings.columns = ['movieId', 'count']
 
-# Singular value decomposition(SVD) --> matrix factorization(Unsupervise Learning)
-u, s, vt = svds(train_data_matrix, k=20)
+duplicated_df = pd.merge(duplicate_movies, duplicate_ratings, on='movieId')
+duplicated_df.sort_values(by=['title', 'count'], ascending=[True, False])
+
+# Removing duplicated ids with low review count
+low_ids = duplicated_df.drop_duplicates(
+    subset='title', keep='last', inplace=False)['movieId']
+movies = movies.loc[~movies['movieId'].isin(low_ids)]
+ratings = ratings.loc[~ratings['movieId'].isin(low_ids)]
+
+df = pd.merge(ratings, movies, on='movieId')
+pivot_df = pd.pivot_table(df, index='title', columns=[
+                          'userId'], values='rating')
+pivot_df = pivot_df.fillna(0)
+sparse_pivot = sparse.csr_matrix(pivot_df)
+
+sparsity = round(
+    1.0-len(df)/(sparse_pivot.shape[0]*sparse_pivot.shape[1]), 3)*100
+print("shape:", sparse_pivot.shape)
+print("sparsity:", str(sparsity)+"%")
+
+# Singular value decomposition(SVD) --> Matrix Factorization
+u, s, vt = svds(sparse_pivot, k=20)
 s_diag = np.diag(s)
-# X(m*n) = USV.T (S(m*n) is a diagonal matrix, features matrix U(users-->m*m) & V(movies)(n*n))
-x_pred = np.dot(np.dot(u, s_diag), vt)
-print(x_pred)
+# S(m*n) is a diagonal matrix, Features Matrix U(users-->m*m) & V(movies)(n*n)
+recommender = np.dot(np.dot(u, s_diag), vt)  # X(m*n) = USV.T
