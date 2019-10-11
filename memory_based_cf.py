bc4c3b0f36f3fc121e5d7b13da596dd82ab80aab
Ashok Thakur <iashok180@gmail.com>
2019-10-11 05:34:00 +0530

    user based titles list added

--- memory_based_cf.py
+++ memory_based_cf.py
@@ -39,28 +39,37 @@ def memory_cf(pivot_df, subject):
     recommender_df = pd.DataFrame(
         recommender, columns=pivot_df.index, index=pivot_df.index)
     cosine_df = pd.DataFrame(
-        recommender_df[subject].sort_values(ascending=False))[1:6]
+        recommender_df[subject].sort_values(ascending=False))
     cosine_df.reset_index(inplace=True)
     return cosine_df
 
 
-user_id = 1
+user_id = 262
 movie_title = 'Toy Story'
+
+# Item based CF
+if movie_title.upper() not in movies['title'].values.tolist():
+    print("Mentioned movie is not in our database")
+else:
+    already_watched = df[df['userId'] == user_id]['title'].values.tolist()
+    item_based_cf = memory_cf(pivot_df, movie_title.upper())
+    print("Films you might enjoy based that you watched {0}:".format(movie_title))
+    for movie in item_based_cf['title'].values.tolist()[1:11]:
+        if movie not in already_watched:
+            print(movie)
+print("-------------------------------------------------------------")
+
+# User based CF
 if user_id not in ratings['userId'].values.tolist():
     print("Mentioned userid is not in our database")
 else:
-
-    if movie_title.upper() not in movies['title'].values.tolist():
-        print("Mentioned movie is not in our database")
-    else:
-        already_watched = df[df['userId'] == user_id]['title'].values.tolist()
-        # Item based CF
-        item_based_cf = memory_cf(pivot_df, movie_title.upper())
-        print("Films you might enjoy based that you watched {0}".format(movie_title))
-        for movie in item_based_cf['title'].values.tolist():
-            if movie not in already_watched:
-                print(movie)
-
-    # User based CF
-    user_based_cf = memory_cf(pivot_df.T, user_id)
-    print(user_based_cf['userId'].values.tolist())
+    already_watched = df[df['userId'] == user_id]['title'].values.tolist()
+    similar_users = memory_cf(pivot_df.T, user_id)
+    similar_users_list = similar_users['userId'].values.tolist()[1:11]
+    similar_user_df = pivot_df[similar_users_list].fillna(0)
+    similar_user_df['mean_rating'] = similar_user_df[similar_users_list].mean(numeric_only=True, axis=1)
+    similar_user_df = similar_user_df.sort_values('mean_rating', ascending=False)
+    print("Flims reccomended for you:")
+    for movie in similar_user_df.index.tolist()[:10]:
+        if movie not in already_watched:
+            print(movie)
