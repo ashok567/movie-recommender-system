ef0314c4000c2f2794d0e608e787442886e048a7
Ashok Thakur <iashok180@gmail.com>
2019-10-12 04:21:36 +0530

    generic approach added

--- content_filtering.py
+++ content_filtering.py
@@ -2,14 +2,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Data Preprocessing
-df = pd.read_csv("data/movie_dataset.csv")
-
-features = ['keywords', 'cast', 'genres', 'director']
-df[features] = df[features].fillna('')
-
-df['title'] = df['title'].str.upper()
-
 
 def combine_features(row):
     try:
@@ -20,25 +12,33 @@ def combine_features(row):
         print(e)
 
 
-df['combined_features'] = df.apply(combine_features, axis=1)
-
-
-# Create count matrix and compute the Cosine Similarity
-def similarity(movie_name):
-    cv = CountVectorizer()
-    count_matrix = cv.fit_transform(df["combined_features"])
-    cosine_sim = cosine_similarity(count_matrix)
-    movie_index = df[df['title'] == movie_name]['index'].values[0]
-    similar_movies = list(enumerate(cosine_sim[movie_index]))
-    similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
-    return similar_movies[1:11]
+# Compute the Cosine Similarity
+def content_similarity(movie_name):
+    similar_movies_list = []
+    df = pd.read_csv("data/movie_dataset.csv")
+    features = ['keywords', 'cast', 'genres', 'director']
+    df[features] = df[features].fillna('')
+    df['title'] = df['title'].str.upper()
+    df['combined_features'] = df.apply(combine_features, axis=1)
+    if movie_name in df['title'].tolist():
+        cv = CountVectorizer()
+        count_matrix = cv.fit_transform(df["combined_features"])
+        cosine_sim = cosine_similarity(count_matrix)
+        movie_index = df[df['title'] == movie_name]['index'].values[0]
+        similar_movies = list(enumerate(cosine_sim[movie_index]))
+        similar_movies = sorted(
+            similar_movies, key=lambda x: x[1], reverse=True)
+        for element in similar_movies[1:50]:
+            similar_movies_list.append(
+                df[df.index == element[0]]["title"].values[0])
+    return similar_movies_list
 
 
 movie_name = input("Enter a movie name\n").upper()
-if movie_name not in df['title'].tolist():
-    print("The movie is not listed in our database")
-else:
+similar_movies_list = content_similarity(movie_name)
+if len(similar_movies_list) > 0:
     print("Also, add these movies to your watch list: ")
-    similar_movies_list = similarity(movie_name)
-    for element in similar_movies_list:
-        print(df[df.index == element[0]]["title"].values[0])
+    for movie in similar_movies_list[:6]:
+        print(movie)
+else:
+    print("Mentioned movie is not our in database")
--- memory_based_cf.py
+++ memory_based_cf.py
@@ -3,12 +3,11 @@ import preprocessing
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 
-df, movies, ratings = preprocessing.data()
-pivot_df = pd.pivot_table(df, index='title', columns=[
-                          'userId'], values='rating')
+user_id = 262
+movie_title = 'abc'
 
 
-def memory_cf(pivot_df, subject):
+def get_cosine_df(pivot_df, subject):
     pivot_df = pivot_df.fillna(0)
     sparse_pivot = sparse.csr_matrix(pivot_df)
     recommender = cosine_similarity(sparse_pivot)
@@ -20,36 +19,56 @@ def memory_cf(pivot_df, subject):
     return cosine_df
 
 
-user_id = 262
-movie_title = 'Toy Story'
-
 # Item based CF
-if movie_title.upper() not in movies['title'].values.tolist():
-    print("Mentioned movie is not in our database")
-else:
-    already_watched = df[df['userId'] == user_id]['title'].values.tolist()
-    item_based_cf = memory_cf(pivot_df, movie_title.upper())
-    print("Films you might enjoy based that you watched {0}:".format(
-        movie_title))
-    for movie in item_based_cf['title'].values.tolist()[1:11]:
-        if movie not in already_watched:
-            print(movie)
+def item_based_cf(user_id, movie_title):
+    df, movies, ratings = preprocessing.data()
+    pivot_df = pd.pivot_table(df, index='title', columns=[
+        'userId'], values='rating')
+    item_movie_df = []
+    if movie_title.upper() in movies['title'].values.tolist():
+        already_watched = df[df['userId'] == user_id]['title'].values.tolist()
+        item_based_cf = get_cosine_df(pivot_df, movie_title.upper())
+        for movie in item_based_cf['title'].values.tolist()[1:]:
+            if movie not in already_watched:
+                item_movie_df.append(movie)
+    return item_movie_df
 
-print("-------------------------------------------------------------")
 
 # User based CF
-if user_id not in ratings['userId'].values.tolist():
-    print("Mentioned userid is not in our database")
+def user_based_cf(user_id, movie_title):
+    df, movies, ratings = preprocessing.data()
+    pivot_df = pd.pivot_table(df, index='title', columns=[
+        'userId'], values='rating')
+    user_movie_df = []
+    if user_id in ratings['userId'].values.tolist():
+        already_watched = df[df['userId'] == user_id]['title'].values.tolist()
+        similar_users = get_cosine_df(pivot_df.T, user_id)
+        similar_users_list = similar_users['userId'].values.tolist()[1:11]
+        similar_user_df = pivot_df[similar_users_list].fillna(0)
+        similar_user_df['mean_rating'] = similar_user_df[similar_users_list].mean(
+            numeric_only=True, axis=1)
+        similar_user_df = similar_user_df.sort_values(
+            'mean_rating', ascending=False)
+        for movie in similar_user_df.index.tolist():
+            if movie not in already_watched:
+                user_movie_df.append(movie)
+    return user_movie_df
+
+
+item_movie_list = item_based_cf(user_id, movie_title)
+if len(item_movie_list) > 0:
+    print("Films you might enjoy based that you watched {0}:".format(
+        movie_title))
+    for movie in item_movie_list[:6]:
+        print(movie)
 else:
-    already_watched = df[df['userId'] == user_id]['title'].values.tolist()
-    similar_users = memory_cf(pivot_df.T, user_id)
-    similar_users_list = similar_users['userId'].values.tolist()[1:11]
-    similar_user_df = pivot_df[similar_users_list].fillna(0)
-    similar_user_df['mean_rating'] = similar_user_df[similar_users_list].mean(
-        numeric_only=True, axis=1)
-    similar_user_df = similar_user_df.sort_values(
-        'mean_rating', ascending=False)
+    print("Mentioned movie is not our in database")
+
+print("-----------------------------------------------")
+user_movie_list = user_based_cf(user_id, movie_title)
+if len(user_movie_list) > 0:
     print("Flims reccomended for you:")
-    for movie in similar_user_df.index.tolist()[:10]:
-        if movie not in already_watched:
-            print(movie)
+    for movie in user_movie_list[:6]:
+        print(movie)
+else:
+    print("Mentioned user is not our in database")
